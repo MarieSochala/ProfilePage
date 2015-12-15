1f279e7efc82e6e7bc35805401664955fb124588
marie <marie.sochala@gmail.com>
2015-12-15 14:37:49 +0100

    ok JS

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,10 @@
-$(document).ready(function(){
-$('.menu').click(function(){
-$(this).fadeout('hide');
+$(document).ready(function(){ 
+
+$('a').on('click',function(){ // quand on clique sur le lien
+
+$('.paragraphe').hide(); // tous les paragraphes sont cachés
+	var toto=$(this).attr('href'); // on appelle le paragraphe lié au lien choisi
+	$(toto).show(); // on ne montre que le paragraphe lié au lien initial
+
 });
 });
