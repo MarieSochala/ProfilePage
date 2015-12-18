5bfa743006cdcc350223f595844d1de74e0786e4
marie <marie.sochala@gmail.com>
2015-12-18 16:08:40 +0100

    ok modal box

--- JS/app.js
+++ JS/app.js
@@ -1,41 +1,37 @@
+"use strict"
 $(document).ready(function(){ 
 
-$('body').on('click','a',function(){ // quand on clique sur le lien
+	$('body').on('click','a',function(event){
+		event.preventDefault(); 
+		$('.paragraphe').hide(); 
+		var toto = $(this).attr('href'); 
+		$(toto).show();
 
-$('.paragraphe').hide(); // tous les paragraphes sont cachés
-	var toto=$(this).attr('href'); // on appelle le paragraphe lié au lien choisi
-	$(toto).show(); // on ne montre que le paragraphe lié au lien initial
+
+		$('.close').on('click', function(){
+			$(GrosseBoite).hide();
+		});
+		// var GrosseBoite=$this.attr('href');
+		// $(GrosseBoite).show();
+
+		// if (toto=='#GrosseBoite'){
+		// 	$('#GrosseBoite').show(); 
+		// } else {
+		// 	$('.paragraphe').hide();
+		// 	$(toto).show();
+		// };
+
+	});
+
+//var url ='https://s.idsympa.com/u-9.json'
+var url ='user-9.json'
+
+$.getJSON(url, function(data){
+	var template = $('#template').html();
+	var info = Mustache.to_html(template, data);
+	$('body').html(info);
+});
 
 });
 
-	// });
-// $(function(){
-	// 	$.getJSON('http://vps227573.ovh.net/u-9.json', function(donnees) {
-	// 		var tata = $('[elt]');
-	// 		for (var index = 0; index < tata.length ; index ++){
 
-	// 		var attribut = $(tata[index]).attr('elt'); // attribut peut etre le first name (clé)
-	// 		var valeur = donnees [attribut];
-	// 		$(tata[index]).html(valeur);
-	// 		}
- // 		});
-
-// $.getJSON('http://vps227573.ovh.net/u-9.json', function(data) {
-// 		   // var FullName = '{{first_name}} {{last_name}}';
-// 		   var FullName = Mustache.to_html('{{first_name}} {{last_name}}', data);
-// 		    //var infoFullName = Mustache.to_html(FullName, data);
-// 		    $('h1').html(FullName);
-
-// 		    var Occupation = Mustache.to_html('{{occupation}}', data);
-// 		    $ ('h3').html(Occupation);
-// 		});
-
- $.getJSON('https://s.idsympa.com/u-9.json', function(data){
- 	var template = $('#template').html();
- 	var info = Mustache.to_html(template, data);
- 	$('body').html(info);
- });
-	});
-    
-
-  
